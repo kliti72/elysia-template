dcaecd2df5c7a28939446ac7407e2b40de9d68bb
kliti elezi <elezikliti@gmail.com>
2026-03-06 15:31:46 +0100

    drizzle run with node

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,10 @@
-// drizzle.config.ts — nella root
 import { defineConfig } from 'drizzle-kit'
-
-const db = Bun.env.DB_PATH ?? './config/database/app.db'
 
 export default defineConfig({
   schema: './config/schema.ts',
   out: './drizzle',
   dialect: 'sqlite',
   dbCredentials: {
-    url: db
+    url: process.env.DB_PATH ?? './config/database/app.db',
   },
 })
